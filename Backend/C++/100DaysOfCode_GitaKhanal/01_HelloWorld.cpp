376b808280d353828ab83ca71c988f5073fcf335
vinisl-dev <vinisl.dev@gmail.com>
2024-01-07 08:03:01 -0300

    started learning of C++, project 100 Days of Code - Gita

---
+++ Backend/C++/100DaysOfCode_GitaKhanal/01_HelloWorld.cpp
@@ -0,0 +1,15 @@
+// include input/output strema libreay used from std::cout
+#include  <iostream>
+//include C standard input/outpu libray :), my first language
+#include <cstdio>
+//this is new for me, allow to use elements from the std namespace without explicitly specifying it.
+using namespace std;
+
+int main(){
+  printf("Hello, World!");
+  return 0;
+} 
+
+// README -  IMPORTANT
+// program compiled for linux/fedora fc38 x86_64
+// gcc 13.2.1

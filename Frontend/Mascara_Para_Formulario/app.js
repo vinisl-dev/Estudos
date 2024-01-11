3c26370b50cf5b9ff277ef01061eb8316d84cec9
vinisl-dev <vinisl.dev@gmail.com>
2024-01-10 21:46:26 -0300

    pequenas alterações no projeto de mascaras

--- Frontend/Mascara_Para_Formulario/app.js
+++ Frontend/Mascara_Para_Formulario/app.js
@@ -1,12 +1,12 @@
 const masks ={
 
   cpf (value){
-return value
-  .replace(/\D/g, '') // não aceita a entrada de letras
-  .replace(/(\d{3})(\d)/,'$1.$2')
-  .replace(/(\d{3})(\d)/,'$1.$2')
-  .replace(/(\d{3})(\d{1,2})/,'$1-$2')
-  .replace(/(-\d{2})\d+?$/,'$1') // limita o campo ao tamanho da regex
+    return value
+      .replace(/\D/g, '') // não aceita a entrada de letras
+      .replace(/(\d{3})(\d)/,'$1.$2')
+      .replace(/(\d{3})(\d)/,'$1.$2')
+      .replace(/(\d{3})(\d{1,2})/,'$1-$2')
+      .replace(/(-\d{2})\d+?$/,'$1') // limita o campo ao tamanho da regex
   },
 
   cnpj(value){
@@ -15,8 +15,31 @@ return value
       .replace(/(\d{2})(\d)/, '$1.$2')
       .replace(/(\d{3})(\d)/, '$1.$2')
       .replace(/(\d{3})(\d)/,'$1/$2')
-      .replace(/(\d{4})(\d{1,2})/,'$1-$2')
-      .replace(/(\d{4}-\d{2})\d+?$/,'$1')
+      .replace(/(\d{4})(\d{1})/,'$1-$2')
+      .replace(/(-\d{2})\d+?$/,'$1')
+  },
+
+  // muito legal porque a mascara é variavel conforme o numero de digitos
+  phone(value){
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d)/,'($1) $2')
+      .replace(/(\d{4})(\d)/,'$1-$2')
+      .replace(/(\d{4,5})(-\d{4})\d+?$/,'$1')
+  
+
+  },
+
+  phoneDDI(value) {
+    return value
+      .replace()
+  },
+
+  cep(value){
+    return value
+      .replace(/\D/g,'')
+      .replace(/(\d{4})(\d)/,'$1-$2')
+      .replace(/(-\d{3})\d+?$/,'$1')
   }
 }
 

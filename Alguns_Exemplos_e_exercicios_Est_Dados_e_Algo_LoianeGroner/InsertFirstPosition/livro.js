1b2fbb93079e2361afd17adc8d58180fceda3d48
vinisl-dev <vinisl.dev@gmail.com>
2024-01-22 19:31:52 -0300

    algoritmo de inclusão sem for não funcionou muito bem

--- Alguns_Exemplos_e_exercicios_Est_Dados_e_Algo_LoianeGroner/InsertFirstPosition/livro.js
+++ Alguns_Exemplos_e_exercicios_Est_Dados_e_Algo_LoianeGroner/InsertFirstPosition/livro.js
@@ -1,5 +1,5 @@
 const { time, timeEnd } = require("console");
-let numbers = [0,1,2,3,4,5,6,7,8,9]
+let numbers = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,]
 
 Array.prototype.insertFirstPosition = function(value) {
   for (let i = this.length; i>= 0; i--){
--- Alguns_Exemplos_e_exercicios_Est_Dados_e_Algo_LoianeGroner/InsertFirstPosition/my.js
+++ Alguns_Exemplos_e_exercicios_Est_Dados_e_Algo_LoianeGroner/InsertFirstPosition/my.js
@@ -1,8 +1,11 @@
-const { time, timeEnd } = require("console");
+ const { time, timeEnd } = require("console");
+
+// isso ficou muito ruim porque conforme o tamanho do array aumenta ele fica mais lento 
+
 let numbers = [0,1,2,3,4,5,6,7,8,9]
 
 
-Array.prototype.MyInsertFirstPosition = function(value) {
+Array.prototype.MyInsertFirstPosition1 = function(value) {
 
   auxArray = new Array(this.length+1)
   auxArray[0]= value
@@ -12,7 +15,31 @@ Array.prototype.MyInsertFirstPosition = function(value) {
 }
 
 
-console.log("my solution: ")
-time()
-numbers.MyInsertFirstPosition(-1)
-timeEnd()
+Array.prototype.MyInsertFirstPosition2 = function(value) {
+
+  auxArray = new Array(this.length+1)
+  auxArray=[value,...this]
+  
+}
+// console.log("my solution: ")
+// time()
+// numbers.MyInsertFirstPosition2(-1)
+// timeEnd()
+// console.log(numbers)
+// console.log(auxArray)
+
+// minha intensão era aproveitar a imutabilidade para sobrescrever array antigo sem usar um laço
+
+Array.prototype.MyInsertFirstPosition3 = function(value) {
+
+  auxArray = new Array(this.length+1)
+  auxArray[0]= value
+  auxArray = this
+  this.length=0
+  this= auxArray
+
+  }
+  time()
+  numbers.MyInsertFirstPosition3(-1)
+  timeEnd()
+  console.log(numbers)

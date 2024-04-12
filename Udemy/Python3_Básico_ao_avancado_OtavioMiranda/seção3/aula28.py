49f5a2ce4c707d4b057c823f7aa4c443cd815863
vinisl-dev <vinisl.dev@gmail.com>
2024-04-12 19:29:45 -0300

    aula28 python udemy, video 2 sql Teo Calvo

---
+++ Udemy/Python3_Básico_ao_avancado_OtavioMiranda/seção3/aula28.py
@@ -0,0 +1,14 @@
+nome = input('Digite seu nome: ')
+idade = input('digite sua idade: ')
+
+if  nome and idade:
+    print(f'seu nome é: ',nome)
+    reverse_nome= nome[::-1]
+    print(f'Seu nome invertido é: ', reverse_nome)
+    name_spaces = nome.__contains__(" ")
+    print ('Seu nome contem espaço ',"sim" if name_spaces == True else "não")
+    print('Seu nome tem {} letras'.format(len(nome)))
+    print('A primeira letra de seu nome é ', nome[0])
+    print('A ultima letra do seu nome é ', nome[-1])
+    splited = nome.split()
+    print(splited)

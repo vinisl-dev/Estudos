17d48c594767a0ceae9f67dd3d48494b64d872e2
vinisl-dev <vinisl.dev@gmail.com>
2024-04-25 20:05:20 -0300

    Revert "projeto cadastro de usuarios Django - muito básico, apenas cadastro e listagem"
    This reverts commit 2b9706ea1248b11aa4b59f9afe70382488699d26.

--- Backend/Python/CRUD_Cad_User_Django_devAprender/projeto_cad_users/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Usuario(models.Model):
-    id_usuario = models.AutoField(primary_key=True)
-    nome = models.TextField(max_length=250)
-    idade = models.IntegerField()
-    
-    
-    

8d70378869aa4b7aa7aef9c1404dcbfbfee60903
vinisl-dev <vinisl.dev@gmail.com>
2024-06-25 14:29:58 -0400

    Aula 3 FastAPI - Conceitos de API e HTTP

--- Python/FastAPI/Dunossauro/fast_zero/fast_zero/app.py
+++ Python/FastAPI/Dunossauro/fast_zero/fast_zero/app.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+from fastapi.responses import HTMLResponse
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code = HTTPStatus.OK, response_model= Message)
 def read_root():
     return {'message': 'Olá mundo'}
+
+
+#     return """
+#     <html>
+#         <header>
+#             <title>Nosso olá mundo!</title>
+#         </header>
+#         <body>
+#             <h1>Olá mundo</h1>
+#         </body>
+#     </html>
+# """

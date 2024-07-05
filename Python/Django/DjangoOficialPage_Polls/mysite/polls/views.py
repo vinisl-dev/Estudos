253b169846cecf344a80391c7090ec3223a39831
vinisl-dev <vinisl.dev@gmail.com>
2024-07-04 22:31:01 -0400

    Projeto Polls - manual oficial django parte 3 - criação dos templates

--- Python/Django/DjangoOficialPage_Polls/mysite/polls/views.py
+++ Python/Django/DjangoOficialPage_Polls/mysite/polls/views.py
@@ -1,14 +1,37 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
 from .models import Question
 
+# Forma completa  de responder uma requisição
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list":latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# forma com atalho shortcut render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+# metodo sem shortcut
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of queston %s."

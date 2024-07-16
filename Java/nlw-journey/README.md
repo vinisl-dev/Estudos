3f6010ecd8dfc265a4b9bce414d654bee55ce2fe
vinisl-dev <vinisl.dev@gmail.com>
2024-07-15 21:15:06 -0400

    Projeto nlw journey - planner em java spring boot

---
+++ Java/nlw-journey/README.md
@@ -0,0 +1,2 @@
+Projeto NLW Journey em Java Spring boot
+criação de um sistema basico de agendamento de viagens
---
+++ Java/nlw-journey/planner/src/main/java/com/rocketseat/planner/participant/ParticpantRequestPayload.java
@@ -0,0 +1,4 @@
+package com.rocketseat.planner.participant;
+
+public record ParticpantRequestPayload(String name, String email) {
+}

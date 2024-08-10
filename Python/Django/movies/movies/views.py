677bd4e7a9efc112708d965bf45b5b10a2a3a3de
vinisl-dev <vinisl.dev@gmail.com>
2024-08-10 08:45:54 -0400

    create template html

---
+++ Python/Django/movies/movies/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+data = {
+'movies':[
+    {
+        'id': 5,
+        'title':'Jaws',
+        'year':1669
+    },
+    {
+        'id': 6,
+        'title':'Sharknado',
+        'year':1600
+    },
+    {
+        'id': 78,
+        'title':'The meg',
+        'year':2000
+    }
+
+]}
+
+def movies(request):
+    return render(request, 'movies/movies.html', data)
+
+
+def home(request):
+    return HttpResponse('home page')
